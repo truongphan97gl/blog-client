e1e7a474435a6b087332f7b5d12703fa6f78b68e
Truong <truongphan97gl@gmail.com>
2019-09-08 19:11:49 -0400

    Fixing production api link

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://quiet-bastion-59588.herokuapp.com/',
+  production: 'https://quiet-bastion-59588.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
